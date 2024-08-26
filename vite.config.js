9c078bd529d0fbd79047b0dc33add69aae1c4830
Adeel <adeeltahir6a@gmail.com>
2024-08-26 16:59:47 -0700

    finally done

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Ensure this matches the directory Vite is using
+  },
   plugins: [react()],
-  base: "/",
+  base: "/upworkpagesproject/",
   resolve: {
     alias: {
       '@': '/src', // Example alias
