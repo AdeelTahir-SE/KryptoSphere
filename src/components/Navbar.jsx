f7ead8757738bc59c76de62e74114c3bce2ef8d5
Adeel <adeeltahir6a@gmail.com>
2024-08-27 01:21:51 -0700

    changes to navbar products

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import logoupworkedited from "../assets/logoupworkedited.png";
 import cart from "../assets/cart.png";
 import hamburger from "../assets/hamburger.png";
@@ -7,15 +7,37 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
 
+    useEffect(() => {
+        if (isDropdownOpen) {
+            dropdownRef.current.classList.add('open');
+        } else {
+            dropdownRef.current.classList.remove('open');
+        }
+    }, [isDropdownOpen]);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsDropdownOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
         <nav>
             <ul className="logo">
-                <li><img src={logoupworkedited} alt="logo" /></li>
+                <li><Link to="/" ><img src={logoupworkedited} alt="logo" /></Link></li>
             </ul>
             <ul className="icons">
                 <li><img src={cart} alt="cart" /></li>
@@ -25,18 +47,15 @@ export default function Navbar() {
                         alt="hamburger" 
                         onClick={toggleDropdown} 
                     />
-                    {isDropdownOpen && (
-                        <div className="dropdown">
-                            <ul>
-                                <li><Link to="/Welcome">Welcome</Link></li>
-                                <li><Link to="/HiddenTreasures">HiddenTreasures</Link></li>
-                                <li><Link to="/HolderRewards">HolderRewarrds</Link></li>
-                                <li><Link to="/Products">Products</Link></li>
-                                <li><Link to="/Contact">Contact</Link></li>
-
-                            </ul>
-                        </div>
-                    )}
+                    <div ref={dropdownRef} className={`dropdown ${isDropdownOpen ? 'open' : ''}`}>
+                        <ul>
+                            <li><Link to="/Welcome">Welcome</Link></li>
+                            <li><Link to="/HiddenTreasures">HiddenTreasures</Link></li>
+                            <li><Link to="/HolderRewards">HolderRewards</Link></li>
+                            <li><Link to="/Products">Products</Link></li>
+                            <li><Link to="/Contact">Contact</Link></li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
         </nav>
